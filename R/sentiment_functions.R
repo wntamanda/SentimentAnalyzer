81a3aa41dfaf07c976d25b9075ab92f53847f1de
Amanda Winata <Amanda.Winata@campus.Imu.de>
2024-07-18 17:28:32 +0200

    Added new function

--- R/sentiment_functions.R
+++ R/sentiment_functions.R
@@ -5,8 +5,8 @@
 #' @examples
 #' calculate_sentiment("I love this product!")
 calculate_sentiment <- function(text) {
-  positive_words <- c("good", "great", "love", "excellent", "happy")
-  negative_words <- c("bad", "worst", "dislike", "terrible", "sad")
+  positive_words <- c("good", "great", "love", "excellent", "happy", "fantastic", "wonderful", "amazing", "pleased", "favorite")
+  negative_words <- c("bad", "worst", "dislike", "terrible", "sad", "horrible", "disappointed", "poor", "hate", "awful")
 
   words <- tolower(unlist(strsplit(as.character(text), "\\s+")))
   score <- sum(words %in% positive_words) - sum(words %in% negative_words)
@@ -21,4 +21,19 @@ calculate_sentiment <- function(text) {
 #' summary_sentiment(c("I love this product!", "I hate this!"))
 summary_sentiment <- function(texts) {
   sapply(texts, calculate_sentiment)
+}
+
+#' Classify Sentiment
+#'
+#' This function classifies the sentiment as positive (1) or negative (0) based on sentiment score.
+#' @param score A numeric value of sentiment score.
+#' @examples
+#' classify_sentiment(3) # Returns 1
+#' classify_sentiment(-1) # Returns 0
+classify_sentiment <- function(score) {
+  if(score > 0) {
+    return(1) # Positive
+  } else {
+    return(0) # Negative
+  }
 }
